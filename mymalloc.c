dcee1fc8c65c749fa79c62c04e7c680544bc45d2
Habeen Jun <hj316@python.cs.rutgers.edu>
2023-02-15 19:07:03 -0500

    first commit

--- mymalloc.c
+++ mymalloc.c
@@ -1,10 +1,36 @@
+#include <stdio.h>
+
 static char memory[4096];
 
 
 void mymalloc(int size) {
-    return pointer;
+
+    // header contains
+    // 1) the size of the chunck
+    // 2) whether the chunk is allocated of free (use the last bit)
+    // instead of using two bytes 
+
+    char byte = memory[0];
+    byte = size;
+    int i;
+    int last_bit = byte & 1;
+
+    for(i = 7; 0 <= i; i --)
+        printf("%d\n", (byte >> i) & 0x01);
+    printf("%d\n", byte & 1);
+    // last_bit = 1;
+
+    
+
+    // printf("%d\n", byte);
+    // printf("%d\n", byte & 1);
+    //return pointer;
 }
 
 void myfree(int *pointer) {
-    return 0;
+    return ;
+}
+
+void main() {
+    mymalloc(3);
 }
