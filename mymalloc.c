4d98a461138db5fc36ebf19a92c5e7524786a060
Habeen Jun <hj316@python.cs.rutgers.edu>
2023-02-22 19:52:48 -0500

    push push

--- mymalloc.c
+++ mymalloc.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mymalloc.h"
+#include <sys/time.h>
 
 #define MEMORY_SIZE 4096
 static char memory[MEMORY_SIZE];
@@ -10,64 +12,136 @@ typedef struct header {
     struct header *next;
 } header;
 
-
-void mymalloc(int size) {
-    
-    head = (header*) memory; 
-
+void* mymalloc(size_t size, char *file, int line)  {
+    header *head = (header*) memory; 
     header *curr = head;
-
+    
+    printf("malloc called! requested size: %ld\n", size);
+    printf("line number: %d", __LINE__);
     while (curr) {
-        if (curr->size == NULL) {
+        printf("current size: %d is_free: %d\n", curr->size, curr->is_free);
+        if (curr->size == 0) {
+            printf("%s", "if\n");
             // first call 
-            curr->size = MEMORY_SIZE - sizeof(header) - size;
+            printf("%s", "firstcall\n");
+            curr->size = size + sizeof(header);
             curr->is_free = 0;
-
-            header *new_chunk = (header*) (curr + sizeof(header) + size);
-            new_chunk->size = MEMORY_SIZE - curr->size;
+            header *new_chunk = (header*) ((void*) curr + sizeof(header) + size); // curr (first memory address of curr) + sizeof(header) + size (requested size) 
+            new_chunk->size = MEMORY_SIZE - sizeof(header) - size;
+            printf("new chunk memory size: %d\n", new_chunk->size);
+        
             new_chunk->is_free = 1;
-
             curr->next = new_chunk;
-            
+
+            return (void*) curr + sizeof(header);
         }
         else if (curr->size >= size + sizeof(header) && curr->is_free == 1) {
+            printf("%s", "else if\n");
             // reqeuested bytes are available to use
-            header *new_chunk = (header*) (curr + sizeof(header) + size);
+            // printf("%s", "not firstcall\n");
+            int availabe_memory = curr->size;
+            printf("available memory: %d\n", availabe_memory);
 
-            curr->next = new_chunk;
-            curr->size = size + sizeof(header);
+            header *new_chunk = (header*) ((void*) curr + sizeof(header) + size);
+            printf("address of new chunk: %p\n", new_chunk);
+            curr->size = sizeof(header) + size;
+            curr->is_free = 0;
+            printf("curr->next: %p\n", curr->next);
 
-            new_chunk->size = MEMORY_SIZE - sizeof(header) - ;
+            printf("new chunck size should be: %ld\n", availabe_memory - sizeof(header) - size);
+            printf("new chunck -> is_free %d\n", new_chunk->is_free);
+            new_chunk->size = availabe_memory - sizeof(header) - size;
+            printf("new chunk memory size: %d\n", new_chunk->size);
+
             new_chunk->is_free = 1; 
+            new_chunk->next = curr->next; // new chunk points to NULL
+            
+            curr->next = new_chunk;
 
-            curr = new_chunk;
+            return (void*) curr + sizeof(header);
 
-        } else {
-            // no available space 
-        }
-
+        } 
         curr = curr->next;
     }
 
-
-    
-    header *ptr;
-    printf("%d", ptr->size);
-
-    
-
-    // header contains
-    // 1) the size of the chunck
-    // 2) whether the chunk is allocated of free 
-
-
-
+    printf("no available space\n line number: %d\n", line);
 }
 
-void myfree(int *pointer) {
-    return ;
+int detect_error(void *pointer) {
+    // first header 
+    header *head = (header*) memory; 
+    header *ptr = head;
+    
+    while (ptr) {
+        if (pointer == ptr && ptr->is_free == 0) {
+            printf("address of user_pointer: %p\n", pointer);
+            printf("address of current_pointer: %p\n", ptr);
+            return 1;
+        }
+
+        ptr = ptr->next;
+    }
+    
+    return -1;
+}
+
+void myfree(void *pointer, char *file, int line) {
+    // getting the chunk header from data pointer.
+    header *curr = (header*) ((void*) pointer - sizeof(header));
+    int result = detect_error(curr);
+    if (result == -1) {
+        printf("error while freeing pointer\n line number: %d\n", line);
+        return;
+    }
+
+    curr->is_free = 1;
+
+    // coalecsing
+    while (curr) {
+        if (curr->next != 0) {
+                if (curr->is_free == 1 && curr->next->is_free == 1) {
+                    curr->size += (sizeof(header) + (curr->next->size));
+                    curr->next = curr->next->next;
+                    }
+        }
+        
+        curr = curr->next;
+    }
+
+    return;
 }
 
 void main() {
-    mymalloc(3);
+    // void *test = malloc(1);
+    // free(test);
+    struct timeval start_time, end_time;
+    // Get the start time
+    gettimeofday(&start_time, NULL);
+    for (int i=0; i < 120; i++) {
+        void *test = malloc(1);
+        free(test);
+    }
+        // Get the end time
+    gettimeofday(&end_time, NULL);
+
+    // Calculate the elapsed time in microseconds
+    long elapsed_time = (end_time.tv_sec - start_time.tv_sec) * 1000000 +
+                        (end_time.tv_usec - start_time.tv_usec);
+    
+    // Convert elapsed time to seconds, if desired
+    double elapsed_time_sec = (double) elapsed_time / 1000000.0;
+    
+    // Print the elapsed time
+    printf("Elapsed time: %ld microseconds (%f seconds)\n", elapsed_time, elapsed_time_sec);
+    
+
+    // void *test = malloc(10); // 1
+    // void *test1 = malloc(10); // 2
+    // void *test2 = malloc(10);
+    // void *test3 = malloc(400);
+    // void *test4 = malloc(400);
+    // void *test5 = malloc(2000);
+
+    // int x;
+    // free(test5);
 }
